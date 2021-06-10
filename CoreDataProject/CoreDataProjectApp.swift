dbbd15f5f4924e01edc55d69ab4bf7d523b781c9
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-06-10 15:31:38 +0500

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Arkasha Zuev on 10.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
